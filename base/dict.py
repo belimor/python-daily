bd4715a5839f1e75c918eb078d5651b41447f779
belimor <dmytro.vaga@gmail.com>
2025-01-09 23:30:57 -0700

    dictionary

--- base/dict.py
+++ base/dict.py
@@ -11,4 +11,21 @@ data = {"name": "Alice", "age": 25, "city": "New York"}
 print(data['name'])
 print(data.get("gender","Not Found"))
 
+data['age']=33
+data['gender'] = 'Female'
+age = data.pop("age")
+print(age)
+print(data)
 
+last_inserted_item = data.popitem()
+print(last_inserted_item)
+print(data)
+
+for key in data:
+    print(key)
+
+for value in data.values():
+    print(value)
+
+for key,value in data.items():
+    print(f"{key}: {value}")
