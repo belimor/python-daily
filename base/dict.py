a7469a69cc9aca3a1c1d3349e3cc75a873840bb6
belimor <dmytro.vaga@gmail.com>
2025-01-09 23:08:24 -0700

    dictionary

---
+++ base/dict.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+# In Python, a dictionary is a mutable, unordered collection of key-value pairs. 
+# Each key is unique, and the values can be of any data type.
+
+# Create e,pty dictionary
+captaines = {}
+cap = dict()
+
+data = {"name": "Alice", "age": 25, "city": "New York"}
+print(data['name'])
+print(data.get("gender","Not Found"))
+
+
