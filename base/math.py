d37089b6b31be7354cca14bd0dd2fca5aca8ff73
belimor <dmytro.vaga@gmail.com>
2025-01-17 14:42:35 -0700

    math

---
+++ base/math.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+
+import math
+
+# Square Root
+print(math.sqrt(16))
+
+# Power
+print(math.pow(2,3))
+
+# Absolute Value
+print(math.fabs(-5))
+
+# Sine, Cosine, Tangent (input in radians)
+print(math.sin(math.pi/2))
+print(math.cos(0))
+print(math.tan(math.pi/4))
+
+# Convert Degrees to Radians
+print(math.radians(180))
+
+# Convert Radians to Degrees
+print(math.degrees(math.pi))
+
+# Natural Logarithm (base e)
+print(math.log(math.e))
+
+# Logarithm with Custom Base
+print(math.log(100,10))
+
+# Logarithm Base 10
+print(math.log10(1000))
+
+# Logarithm Base 2
+print(math.log2(8))
+
+# Factorial
+print(math.factorial(5))
+
+# Rounding Functions
+print(math.floor(3.7))
+print(math.ceil(3.7))
+
+# Truncate (remove decimal part)
+print(math.trunc(4.9))
+
+# Pi and Euler's Number
+print(math.pi)
+print(math.e)
+
+# Infinity and NaN
+print(math.inf)
+print(math.nan)
+
+
+
+
