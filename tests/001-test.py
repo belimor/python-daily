b231f01b782ea36bb535831daf64d160bc564497
belimor <dmytro.vaga@gmail.com>
2025-01-20 16:02:44 -0700

    001 test

--- tests/001-test.py
+++ tests/001-test.py
@@ -1,9 +1,7 @@
 #!/bin/python3
 
-
 #
 # Complete the 'numCells' function below.
-#
 #
 
 def numCells(grid):
@@ -11,14 +9,6 @@ def numCells(grid):
     count = 0
     for i in range(len(mg)):
         for j in range(len(mg[i])):
-            print("= = = =")
-            print(mg[i][j],i,j)
-            if i == 0 and j == 0:
-                if mg[i][j] > mg[i][j+1] and mg[i][j] > mg[i+1][j+1] and mg[i][j] > mg[i+1][j]:
-                    count += 1
-            if i == (len(mg)-1) and j == (len(mg[i])-1):
-                if mg[i][j] > mg[i][j-1] and mg[i][j] > mg[i-1][j-1] and mg[i][j] > mg[i-1][j]:
-                    count += 1
             if i == 0: a = 0 
             else: a = -1
             if j == 0: b = 0
@@ -30,22 +20,24 @@ def numCells(grid):
             bcount = True
             for x in range(a,c+1):
                 for y in range(b,d+1):
-                    if y != 0 or x != 0:
-                        print("x:",x,"y:",y)
-                        if mg[i][j] > mg[i+x][i+y]:
+                    if y == 0 and x == 0:
+                        pass
+                    else:
+                        if mg[i][j] > mg[i+x][j+y]:
                             bcount = bcount and True
                         else:
                             bcount = bcount and False
-            print(bcount)
-
-    print(mg)
+            if bcount: 
+                count += 1
+                print("YES:",mg[i][j],i+1,j+1)
     return count
 
 if __name__ == '__main__':
 
-    grid = [[9,2,3,4],[1,2,3,4],[3,4,5,6]]
+    grid = [[9,2,3,4],[1,2,8,4],[3,4,5,6]]
 
-    print(grid)
+    for i in range(len(grid)):
+        print(grid[i])
     result = numCells(grid)
     print(result)
 
