3679fbd6fc41e0ccedd4d773162871d951afdc51
Dmitry <dmytro.vaga@gmail.com>
2025-02-01 15:07:03 -0700

    script updated and tested

--- sysadmin_scripts/003-chatgpt.py
+++ sysadmin_scripts/003-chatgpt.py
@@ -1,40 +1,42 @@
 #!/usr/bin/python3
 # https://github.com/openai/openai-python
 
-import openai
 import os
 
 # Set your OpenAI API key
 # export OPENAI_API_KEY="your_api_key"
-api_key = os.getenv("OPENAI_API_KEY")
+from openai import OpenAI
 
-if api_key:
-    openai.api_key = api_key
+my_api_key = os.getenv("OPENAI_API_KEY")
+
+if my_api_key:
+  client = OpenAI(
+    api_key = my_api_key
+  )
 else:
     print("OPENAI_API_KEY is not set")
+    exit()
 
 def chat_with_gpt(prompt, model="gpt-4o-mini"):
     try:
         # Make the API call
-        response = openai.chat.completions.create(
+        completion = client.chat.completions.create(
+                store = True, #enable the storage of chat completion outputs for future reference
                 model = model,
                 messages = [
-                    {"role": "system", "content:": "You are a helpful assistant."},
-                    {"role": "user", "content:": prompt}
+                  {"role": "system", "content": "You are a helpful assistant."},
+                  {"role": "user", "content": prompt}
                 ],
                 temperature = 0.7,    # Adjust creativity level (0-1)
-                max_tokens = 100,     # Adjust token limit
-                top_p = 1,            # Nucleus sampling
-                frequency_penalty=0, # Penalize frequent words
-                presence_penalty=0   # Penalize topics repetition
+                max_tokens = 100     # Adjust token limit
         )
         # Extract and return the response
-        return response['choices'][0]['message']['content']
+        return completion.choices[0].message.content 
     except Exception as e:
         return f"An error occured {e}"
 
 if __name__ == "__main__":
     prompt = input("Enter your prompt: ")
     reply = chat_with_gpt(prompt)
-    print(f"\nChatGTP:\n{reply}")
+    print(f"ChatGTP:\n{reply}\n")
 
