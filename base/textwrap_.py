445ee76063d43b8d71fb7a0804a667ae0e3bd9ae
Dmitry <dmytro.vaga@gmail.com>
2025-02-03 12:48:01 -0700

    textwrap

--- base/textwrap_.py
+++ base/textwrap_.py
@@ -9,5 +9,45 @@ import textwrap
 
 text = "Python is an amazing programming language. It is versatile and easy to learn."
 wrapped_text = textwrap.wrap(text, width=30)
-
 print(wrapped_text)
+print("\n".join(wrapped_text))
+
+# Formatting Text with textwrap.fill()
+
+formatted_text = textwrap.fill(text,width=30)
+print(formatted_text)
+
+# Adding Indents with textwrap.indent()
+
+indented_text = textwrap.indent(formatted_text, prefix=">> ")
+print(indented_text)
+
+# Removing Indents with textwrap.dedent()
+
+indented_text = """
+   Python is fun.
+   This text has leading spaces
+"""
+dedented_text = textwrap.dedent(indented_text)
+print(dedented_text)
+
+# Controlling Subsequent Line Indents with textwrap.TextWrapper
+
+wrapper = textwrap.TextWrapper(width=30, initial_indent="==> ", subsequent_indent="    ")
+formatted_text = wrapper.fill(text)
+print(formatted_text)
+
+# Shortening Text with textwrap.shorten()
+
+short_text = textwrap.shorten(text, width=25, placeholder="...")
+print(short_text)
+
+# Customizing Wrapping Behavior
+
+wrapper = textwrap.TextWrapper(width=20, expand_tabs=False, replace_whitespace=True)
+custome_wrapped_text = wrapper.wrap("Python\tis\tawesome\tand\tfun!")
+print(custome_wrapped_text)
+
+
+
+
