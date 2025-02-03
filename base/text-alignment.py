005c821c20ab47f965ad080901f2c601ad9d811b
Dmitry <dmytro.vaga@gmail.com>
2025-02-03 09:50:04 -0700

    text alignment

---
+++ base/text-alignment.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# In Python, a string of text can be aligned left, right and center.
+
+width = 40
+print('HackerRank'.ljust(width,'-'))
+
+print('HackerRank'.center(width,'-'))
+
+print('HackerRank'.rjust(width,'-'))
