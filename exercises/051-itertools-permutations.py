ded07d8183103f279ead8319179cda62659fbddd
Dmitry <dmytro.vaga@gmail.com>
2025-02-10 09:45:19 -0700

    exercise 051

---
+++ exercises/051-itertools-permutations.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python3
+
+# itertools.permutations(iterable[, r])
+
+# This tool returns successive r length permutations of elements in an iterable.
+
+# If r is not specified or is None, then  defaults to the length of the iterable, and all possible full length permutations are generated.
+
+# Permutations are printed in a lexicographic sorted order. So, if the input iterable is sorted, the permutation tuples will be produced in a sorted order.
+
+# Sample Code
+
+# >>> from itertools import permutations
+# >>> print permutations(['1','2','3'])
+# <itertools.permutations object at 0x02A45210>
+# >>> 
+# >>> print list(permutations(['1','2','3']))
+# [('1', '2', '3'), ('1', '3', '2'), ('2', '1', '3'), ('2', '3', '1'), ('3', '1', '2'), ('3', '2', '1')]
+# >>> 
+# >>> print list(permutations(['1','2','3'],2))
+# [('1', '2'), ('1', '3'), ('2', '1'), ('2', '3'), ('3', '1'), ('3', '2')]
+# >>>
+# >>> print list(permutations('abc',3))
+# [('a', 'b', 'c'), ('a', 'c', 'b'), ('b', 'a', 'c'), ('b', 'c', 'a'), ('c', 'a', 'b'), ('c', 'b', 'a')]
+
+# Task
+
+# You are given a string S.
+# Your task is to print all possible permutations of size k of the string in lexicographic sorted order.
+
+# Input Format
+
+# A single line containing the space separated string S and the integer value k.
+
+# Constraints
+
+# 0 <= k <= len(S)
+
+# The string contains only UPPERCASE characters.
+
+# Output Format
+
+# Print the permutations of the string S on separate lines.
+
+mline = """
+# Sample Input
+# HACK 2
+# Sample Output
+# AC
+# AH
+# AK
+# CA
+# CH
+# CK
+# HA
+# HC
+# HK
+# KA
+# KC
+# KH
+"""
+print(mline)
+
+# Explanation
+
+# All possible size  permutations of the string "HACK" are printed in lexicographic sorted order.
+
+from itertools import permutations
+A = input().split()
+my_string = A[0]
+r = int(A[1])
+result = []
+product = list(permutations(my_string, r))
+for x in product:
+    new_str = ""
+    for y in x:
+        new_str += y
+    result.append(new_str)
+result.sort()
+for r in result:
+    print(r)
+
+
+# >>> from itertools import permutations
+# >>> print permutations(['1','2','3'])
+# <itertools.permutations object at 0x02A45210>
+# >>> 
+# >>> print list(permutations(['1','2','3']))
+# [('1', '2', '3'), ('1', '3', '2'), ('2', '1', '3'), ('2', '3', '1'), ('3', '1', '2'), ('3', '2', '1')]
+# >>> 
+
+
+
